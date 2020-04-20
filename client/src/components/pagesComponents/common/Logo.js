6a7c9d23dd4e9ab9452f2cba30aaf6e65f34d95e
Elbarae <elbarae1921@gmail.com>
2020-04-20 10:28:37 +0000

    Changed logo

--- client/src/components/pagesComponents/common/Logo.js
+++ client/src/components/pagesComponents/common/Logo.js
@@ -11,7 +11,7 @@ export class Logo extends Component {
                 <Link to="/about" className="abb">
                     <div>
                         <div id="abb">
-                            CNC
+                            NCC
                         </div>
                         <div id="title">
                             National Catastrophe Center
