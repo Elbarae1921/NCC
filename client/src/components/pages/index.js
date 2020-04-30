7c0032837d19469ed3bf71ec0d9cc67e10ba6b6b
Elbarae1921 <elbarae1921@gmail.com>
2020-04-30 23:36:21 +0000

    Changed folder structure and switched to functional components

---
+++ client/src/components/pages/index.js
@@ -0,0 +1,9 @@
+export { CheckIn, CheckInConfirmation } from './CheckIn';
+export { Find, FindResults } from './Find';
+export { Index } from './Index';
+export { Partner } from './Partner';
+export { Staff} from './Staff';
+export { Weather, WeatherResults } from './Weather';
+export { About } from './About';
+export { Contact } from './Contact';
+export { NotFound } from './NotFound';
